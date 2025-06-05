e550cbd4281ad522aada767e86645dd1c6cce4fe
mentalpirate <somewhere@gmail.come>
2025-06-05 10:38:33 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Normal Route 
+@app.route("/help")
+def help():
+    return render_template("help.html")
+
+# Http method
+@app.post("/data")
+def data():
+    if request.method == "POST":
+        return f"Data has been successfully submitted {request.form['data']}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
